113c3ff88c23f7bfa3dc5f737c6c8df95ab08a40
tokams <t.gliniecki@hotmail.com>
2016-01-27 20:23:55 +0100

    configured LED uis slack

--- config.js
+++ config.js
@@ -1,18 +1,18 @@
 module.exports = {
   // your community or team name to display on join page.
-  community: process.env.COMMUNITY_NAME || 'YOUR-TEAM-NAME',
+  community: process.env.COMMUNITY_NAME || 'LEDUIS',
   // your slack team url (ex: socketio.slack.com)
-  slackUrl: process.env.SLACK_URL || 'YOUR-TEAM.slack.com',
+  slackUrl: process.env.SLACK_URL || 'LEDUIS.slack.com',
   // access token of slack
   // You can generate it in https://api.slack.com/web#auth
   // You should generate the token in admin user, not owner.
   // If you generate the token in owner user, missing_scope error will be occurred.
   //
   // You can test your token via curl:
-  //   curl -X POST 'https://YOUR-SLACK-TEAM.slack.com/api/users.admin.invite' \
+  //   curl -X POST 'https://LEDUIS.slack.com/api/users.admin.invite' \
   //   --data 'email=EMAIL&token=TOKEN&set_active=true' \
   //   --compressed
-  slacktoken: process.env.SLACK_TOKEN || 'YOUR-ACCESS-TOKEN',
+  slacktoken: process.env.SLACK_TOKEN || 'xoxp-19212752343-19229859125-19604438755-652fd4f2af',
   // an optional security measure - if it is set, then that token will be required to get invited.
   inviteToken: process.env.INVITE_TOKEN || null
 };
